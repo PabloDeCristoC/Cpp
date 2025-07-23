112b13859042e18782ae4f8a0effb003f16edc09
Pablo Emanuel de Cristo Cavieres Körn <pablocaviereskorn@gmail.com>
2025-07-23 19:15:24 -0400

    solo config

--- Extra/Practica Vacaciones 2025/practica.cpp
+++ Extra/Practica Vacaciones 2025/practica.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 
+bool ParNegativo(int n1){
+    bool result = false; 
+    if(n1%2 == 0 && n1){
+         result = true;
+    }
+    return result;
+}
+
+bool DivisionExacta(int n1, int n2){
+    bool result = false;
+    if (n1%n2 == 0){
+        result = true;
+    }
+    return result;
+}
+
+
 using namespace std;
 
 int main(){
-    cout<<"Hola mundo";
+    int n1, n2;
+    cout<<"Ingrese el numero n1: "; cin>>n1;
+    cout<<"Ingrese el numero n2: "; cin>>n2;
+    cout<<ParNegativo(n1);
+    cout<<DivisionExacta(n1, n2);
     return 0;
 }
